8ce441d92c8a3c3da7bd7371fd9bd6c28f89fd42
Marcel Wagner <info@marcelwagner.dev>
2025-07-21 17:19:23 +0200

    enable darkmode

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"  class="dark">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,6 +12,10 @@
         <style>
             html {
                 background-color: oklch(1 0 0);
+            }
+
+            html.dark {
+                background-color: oklch(0.145 0 0);
             }
         </style>
 
